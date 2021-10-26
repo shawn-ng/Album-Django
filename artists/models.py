25fc2d194013a77f49841a81bdd054cd8b32c4a0
Shawn-NG <nghengdi10@gmail.com>
2021-10-26 18:42:38 +0100

    created artist model

--- artists/models.py
+++ artists/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=50, default=None)
+    members = models.ManyToManyField(
+        to='ArtistMember', related_name='artists', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ArtistMember(models.Model):
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(max_length=50)
+
+    def __str__(self):
+        return self.name
