ffa96cb8e2eb1aad5ea3738c621b3b2c13848aa8
Shawn-NG <nghengdi10@gmail.com>
2021-10-26 18:58:47 +0100

    changed the model of the albums and artists

--- albums/models.py
+++ albums/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+from artists.models import Artist
+
 # Create your models here.
 
 
 class Albums(models.Model):
     title = models.CharField(max_length=50)
-    artist = models.CharField(max_length=50)
     # you can store those stastic image in a static server.
     cover_image = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.
+                               CASCADE, default=None, null=True)
 
     def __str__(self):
         return f'{self.title} by {self.artist} ({self.id})'
